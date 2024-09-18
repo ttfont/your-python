9816d55e154d1661a59e0f2b1635a627968cf4e4
ttfont <ttfont>
2024-09-18 09:14:55 +0800

    不太实用的计算器.py 文件批量处理.py

---
+++ project/不太实用的计算器.py
@@ -0,0 +1,80 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    # 项目实践 不太实用的计算器
+    # 做个小项目，把前面学到的知识用起来
+    # 发现问题、分析问题、解决问题、总结复盘
+    # 预备课:
+    # class类
+    # 变量与运算的数学运算
+    # 数据种类
+    # for 和 while 循环
+
+    # 假如现在你手上没有计算器，需要设计一个计算器（发现问题）
+
+    # 产品定位
+    # 计算两个数的加减乘除等一些常用方法
+    # 一次性计算一批数据的运算结果
+
+    # 程序设计
+    # 程序设计出来要批量化生成，需要把封装在一起，标准化，然后批量生产出来，这个需要前面学到的知识 class类
+
+
+# 变量与运算的数学运算
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        # 这里我们得处理一个异常
+        if b == 0:
+            print("b cannot be 0")
+        else:
+            return a / b
+
+# 使用计算器
+c = Calculator()
+print(c.add(1, 2))
+print(c.divide(1, 2))
+c.divide(1, 0)
+
+
+# 批量计算：数据种类，for 和 while 循环，和继承
+class Calculator01(Calculator):
+    def subtract(self, a, b):
+        return super().subtract(a, b)
+
+    def batch_subtract(self, a_list, b_list):
+        res_list = []
+        for i in range(len(a_list)):
+            res_list.append(self.subtract(a_list[i], b_list[i]))
+        return res_list
+
+
+c01 = Calculator01()
+print(c01.subtract(2, 1))
+# 批量计算 [3, 2, 1] 和 [3, 2, 1] 的差
+print(c01.batch_subtract([3, 2, 1], [2, 3, 4]))
+
+# 本文并没有完成所有的计算器功能，因为本文的目标是通过一个简单的示例，来展示完整的思考流程，
+# 而不仅仅是制作一个工具。从发现问题、分析问题、解决问题，到简化问题和代码重用，
+# 本文希望用最基本的例子贯穿整个过程。这样，即使你以后面对更复杂、
+# 更困难的挑战，也能运用这套思路，一步一步地解决问题满足需求。
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('不太实用的计算器')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ project/文件批量处理.py
@@ -0,0 +1,86 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import os
+import re
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    # 项目实践 文件批量处理
+    # 预备课：
+    # 文件目录管理
+    # for 和 while 循环
+    # 读写文件
+    # 正则表达式
+
+    # 主要涉及功能
+    # 找到所有文件
+    # os.listdir()
+    # os.path.join()
+    # 找到文件特定字段
+    # re.findall()
+    # os.path.join()
+    # 替换
+    # os.path.join()
+    # re.sub()
+    # string.startswith()
+
+    # 我遇到的问题 ，我们要处理大量文件 比如 1000 个文件，需要替换其中的特定字符，
+    # 比如把 morvanzhou.github.io 改成 mofanpy.com
+
+    # 分析问题
+    # 遍历所有的文本文件
+    # 找到文件中特定字段
+    # 替换掉这个特定字段
+
+    # 找到所有文件，文件目录管理
+    print(os.listdir("yourfiles"))
+
+    # 找到文件特定字段,for 和 while 循环,读写文件
+    for filename in os.listdir("yourfiles"):
+        file_path = os.path.join("yourfiles", filename)
+        with open(file_path, "r") as f:
+            print(file_path, ": ", f.read())
+
+    # 找出复杂的字符串，正则表达式
+    string = "这是我的主页 https://mofanpy.com, 这个 www.mofanpy.com 有很多 mofan 教你机器学习和 python 语言的教学"
+    res = re.findall(r"(https://)?(mofanpy.com)", string)
+    for r in res:
+        print(r[1])
+
+    # 替换，有二种方案
+    # 在原文本上替换，并覆盖原文本的内容；
+    # 复制出一个新的文件，将原文本替换过的文字拷贝到新文件中，原文件不改变。
+    # 这里选择方案二
+    for filename in os.listdir("yourfiles"):
+        file_path = os.path.join("yourfiles", filename)
+        with open(file_path, "r") as f1:
+            string = f1.read()
+            new_string = re.sub(r"morvanzhou.github.io", "mofanpy.com", string)
+            with open(os.path.join("yourfiles", "new_" + filename), "w") as f2:
+                f2.write(new_string)
+
+    # 查看文件是否正确
+    print()
+    for filename in os.listdir("yourfiles"):
+        if filename.startswith("new_"):
+            continue
+        file_path = os.path.join("yourfiles", "new_" + filename)
+        with open(file_path, "r") as f:
+            print(file_path, ": ", f.read())
+
+# 尽管这个任务看起来很简单，但其实包含了对项目的一些深度思考，
+# 比如如何发现问题、分析问题、解决问题。通过之前那个不太实用的计
+# 算器项目以及这个练习，大家对使用 Python 开发项目应该有了更深入的理解。
+#
+# 好了，到了这个阶段，大家已经可以尝试独立完成一些简单的项目了。
+# 接下来的课程将致力于进一步提升你的技能，而且内容会更加丰富精彩。
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('文件批量处理')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
